40d0403fb081acf42c9b9000cf3a6783b9b306f8
Trish Griego <pmgriego@gmail.com>
2016-11-16 11:18:04 -0800

    Beyond set up, ensuring that function highestScoreFrom returns correctly scored - highest word.

--- scrabble.js
+++ scrabble.js
@@ -1,12 +1,5 @@
 var Scrabble = function() {};
 
-// YOUR CODE HERE
-// Scrabble.prototype.helloWorld = function() {
-//
-//   return 'hello world!';
-//
-// };
-
 Scrabble.prototype = {
   helloWorld: function() {return "Hello World!"},
 
@@ -53,21 +46,25 @@ Scrabble.prototype = {
     for(var i = 0; i < word.length; i ++) {
 
       letter = word[i];
-      // console.log("letter ",letter);
+      console.log("letter ",letter); // c a t
 
       score += alphabet[letter];
       // // console.log("alphabet",alphabet, alphabet[letter]);
       // console.log("score: ", score, word);
     } // word loop end.
     // console.log("You Played:", word ,", worth: ", score, "points");
+    // "You Played" [cat] worth: [5] points
     return score;
 
   }, //score function end
 
+  // highestScoreFrom(arrayOfWords): returns the word in the array with the highest score.
   highestScoreFrom: function(words) {
+    // create initial holder for score.
     var max_score = 0;
+    // score the current_word.
     var current_word = this.score(words[0]);
-    console.log(current_word);
+    console.log("current_word: ",current_word);
 
     for(var i = 0; i < words.length; i++) {
       // console.log(words);
@@ -75,7 +72,7 @@ Scrabble.prototype = {
 
       if (max_score > current_word[i]) {
 
-        console.log(words[i]);
+        console.log('words', words[i]);
         return max_score;
       }
 
@@ -93,22 +90,46 @@ Scrabble.prototype = {
 }; // scrabble end
 
 
-
+// create a scrabble object?
 var myScrabble = new Scrabble();
 
-// console.log(myScrabble.helloWorld());
+// Test 1.
+/* score(word): returns the total score value for the given word. The word is input as a string (case insensitive). The chart below shows the point value for a given letter. */
+// tests checked on lines 52-56.
+myScrabble.score("cat");
 
-myScrabble.score("CAT");
+
+// Test 2.
+/* highestScoreFrom(arrayOfWords): returns the word in the array with the highest score. */
+// sizzles = 25, fuzz = 25, dog = 5, cat = 5. enchanted = 15
+// create a collection of words.
+var collection = ["sizzles", "enchanted", "dog", "cat"];
+// using new function name. I should return the highest scored word in array.
+ // which in this case is sizzles.
+myScrabble.highestScoreFrom("I should return sizzles", collection);
+
+// => should return sizzles as highestscorefrom
+
+//WORD KEY:
+// biz = 14, quasi = 14 , cat = 5, milk = 10
+
+
 
 // Previous test example of what it make look like.
+
+
 // it 'tie between words of length 7 and another word of smaller length with same point value.' do
 //   expect(Scrabble::Scoring.highest_score_from(["sizzles", "fuzz", "dog", "cat"])).must_equal("sizzles")
 // end
 
 
-var collection = ["sizzles", "fuzz", "dog", "cat"];
-myScrabble.highestScoreFrom(collection);
-// => should return sizzles as highestscorefrom.
+// it 'Two word tie goes to the word with fewest letters' do
+//   expect(Scrabble::Scoring.highest_score_from(["biz","quasi", "cat", "milk"])).must_equal("biz")
+// end
+
+
+
+
 
 
 
